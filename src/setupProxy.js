f94cf1040aa324e197b107b287ba040eafe61039
tigrade <tigrawise@gmail.com>
2022-09-30 00:04:49 +0800

    1

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,8 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(createProxyMiddleware('/api', { 
-      target: 'http://localhost:3100/',
+      // target: 'http://192.168.3.131:3100/',
+      target: 'http://127.0.0.1:3100/',
       changeOrigin: true,
       pathRewrite: {
         "^/api": "/"
